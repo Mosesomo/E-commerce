508390d1fd393bb6193b44313115e2dfc671f2d2
Mosesomo <moseswamboga4@gmail.com>
2023-04-29 00:08:35 +0300

    all about coding

---
+++ ecom.js
@@ -0,0 +1,10 @@
+function openmenu(){
+    document.getElementById("stocks-menu").style.display="block";
+    document.getElementById("open-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+function closemenu(){
+    document.getElementById("stocks-menu").style.display="none";
+    document.getElementById("open-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
